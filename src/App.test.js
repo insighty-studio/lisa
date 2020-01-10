50494372af62becd29c123fcb4c197bebc664e12
nebelyng <nebelyng@mail.ru>
2020-01-10 19:00:59 +0200

    Setup ESLint

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const  div = document.createElement('div');
+  const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 
   expect(div.innerHTML).toContain('Just test =)');
